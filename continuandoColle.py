c6a21fb8ab31ebee9eac431faaf96426fd02d9f4
Rodrigo Rafa <rodrigorafa71@gmail.com>
2022-02-05 16:24:45 -0300

    Inserindo for usuario

--- continuandoColle.py
+++ continuandoColle.py
@@ -2,6 +2,9 @@ usuarios_data_science = [15, 23 , 43, 56]
 usuarios_machine_learrning = [13, 23, 56, 42]
 
 assistiram = usuarios_data_science.copy()
-set(assistiram)
-print(assistiram)
+assistiram.extend(usuarios_machine_learrning)
 
+for usuario in set(assistiram):
+    print(usuario)
+
+#set([1, 2, 3, 1]) - imprime {1, 2, 3}
