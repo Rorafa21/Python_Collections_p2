e54e77010d1e1d1f726119a8c2958438f8eef75e
Rodrigo Rafa <rodrigorafa71@gmail.com>
2022-02-05 17:58:40 -0300

    Comentarios

--- dictmapa.py
+++ dictmapa.py
@@ -1,10 +1,12 @@
 aparicoes = {
-    "Rodrigo" : 2,
-    "Rafa" : 4,
-    "cachorro" : 1,
-    "nome" : 1,
+    "Rodrigo" : 2, #KEYs : VALUEs   |
+    "Rafa" : 4,                    #|
+    "cachorro" : 1,                # Items 
+    "nome" : 1,                    #|         
     
 }
 
-aparicoes["Carlos"] = 1
+aparicoes["Carlos"] = 1 #del aparicoes["Carlos"] remove o dado carlos do dict
 print(aparicoes)
+
+
