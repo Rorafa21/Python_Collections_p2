1d8b2c7fbb104967d3e6a15dc7c002c2685e2554
Rorafa21 <rodrigorafa71@gmail.com>
2022-02-07 13:03:10 -0300

    tirando print

--- defaultdict.py
+++ defaultdict.py
@@ -3,7 +3,6 @@ from unittest import defaultTestLoader
 
 meu_texto = "Bem vindo meu nome é rodrigo eu gosto muito de nome e tenho o meu cachorro e gosto muito de cachorro"
 meu_texto = meu_texto.lower()
-print(meu_texto.split())
 aparicoes = defaultdict()
 
 for palavra in meu_texto.split():
