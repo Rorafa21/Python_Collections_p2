7545cb090541d52e6a4d39ebbbaf57fa6ee911be
Rorafa21 <rodrigorafa71@gmail.com>
2022-02-07 13:07:39 -0300

    defaultdict(int)

--- defaultdict.py
+++ defaultdict.py
@@ -3,7 +3,7 @@ from unittest import defaultTestLoader
 
 meu_texto = "Bem vindo meu nome é rodrigo eu gosto muito de nome e tenho o meu cachorro e gosto muito de cachorro"
 meu_texto = meu_texto.lower()
-aparicoes = defaultdict()
+aparicoes = defaultdict(int)
 
 for palavra in meu_texto.split():
     aparicoes[palavra] += 1
